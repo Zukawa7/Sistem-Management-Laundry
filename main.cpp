077fd0dd4c7d3f064ca5e4610169df8d58a1d6b4
Zukawa7 <2500018236@webmail.uad.ac.id>
2026-07-07 20:51:48 +0700

    sync user login to struct global

--- main.cpp
+++ main.cpp
@@ -1,24 +1,37 @@
 	#include<iostream>
+	#include<fstream>
 	using namespace std;
 	
 	void cls(){ cout << "\033[2J\033[1;1H"; } //Untuk clear Screen
 	void key(){ cin.ignore();cin.get();}
-	class Login {
-		friend ostream& operator <<(ostream&,const Login&);
-	    friend istream& operator >>(istream&, Login&);
+	class SystemLaundry {
+		friend ostream& operator <<(ostream&,const SystemLaundry&);
+	    friend istream& operator >>(istream&, SystemLaundry&);
 	    
 	    public : 
 	    	bool getLoggedIn() {
             return LoggedIn; 
         }
+        SystemLaundry(){
+        	userData[userCount].UserName = "ucok";
+        	userData[userCount].UserPw = "123";
+	}
+        
 	    private :
-	    	string UserName,UserPw;
+			int userCount = 0;
+			struct user {
+				string UserName,UserPw;
+				bool typeAccount;
+			};
+			string PIN = "12345";
+			user userData[1000];
 	    	void ProcessLogin();
 	    	void AccLogin();
 	    	void AccReg();
+			void logout();
 	    	bool LoggedIn = false;
 	};
-	
+
 	class Menu{
 		
 		public :
@@ -26,17 +39,45 @@
 			
 		private :
 			void UnderConstruction();
+			void inputPelanggan();
+			void updateStatus();
+			void dataTransaksi();
+			void logout();
 			int Input;
+			struct client {
+				string nama, hp, id, tgl, paket, status;
+    			int berat,harga;
+    			long total; 
+			};
+			struct harga{
+				int cuci_kering, setrika, ekspres, reguler; 
+			};
+			client clinetData[1000];
+			harga fees[1000];
 	};
-	
-	istream& operator>>(istream& input, Login& Acc){
+			
+		
+	istream& operator>>(istream& input, SystemLaundry& Acc){
 		Acc.ProcessLogin();
 	    return input;
 	}
-	ostream& operator<<(ostream& output,const Login& Acc){
+	ostream& operator<<(ostream& output,const SystemLaundry& Acc){
 
 	    return output;
 	}
+	
+	class fileManagement{
+		friend class Menu;
+		friend class SystemLaundry;
+		public : 
+		
+		
+		
+		private :
+
+				
+			
+	};
 	
 	
 	void Menu::Process(){
@@ -54,22 +95,22 @@
 				case 1 :
 					cls();
 					UnderConstruction();
-					//Untuk menu input data pelanggan
+					inputPelanggan();
 					break;
 				case 2 :
 					cls();
 					UnderConstruction();
-					//Unutk menu update status
+					updateStatus();
 					break;
 				case 3 :
 					cls();
 					UnderConstruction();
-					//Untuk menu Data transaksi
+					dataTransaksi();
 					break;
 				case 4 :
 					cls();
 					UnderConstruction();
-					//Logout menu
+					logout();
 					break;
 				default :
 					cls();
@@ -78,8 +119,25 @@
 		}while(Input != 4);
 	} 
 	
+	void Menu::updateStatus(){
 	
-	void Login::ProcessLogin(){
+	}
+
+	void Menu::inputPelanggan(){
+	
+	}
+
+	void Menu::dataTransaksi(){
+	
+	}
+
+	void Menu::logout(){
+	
+	}
+
+
+	
+	void SystemLaundry::ProcessLogin(){
 		char i;
 		cout << "Apakah Anda Sudah Memiliki Akun? (Y/N) : ";
 		cin >> i;
@@ -91,15 +149,17 @@
 			AccReg();
 			AccLogin();
 			LoggedIn = true;
-		} else {
+		} else if (!i){
 			cout << "Hanya Masukkan Input Y/N!\n";
 			ProcessLogin();
+		} else {
+			key();
 		}
 	}
 	
-	void Login::AccLogin(){
+	void SystemLaundry::AccLogin(){
 		string InName,InPw;
-	
+		do{
 			cout << "+=======================+" << endl;
 	    	cout << "|      LOGIN AKUN       |" << endl;
 	    	cout << "+=======================+" << endl;
@@ -107,30 +167,54 @@
 			cin >> InName;
 			cout << "Masukkan Password : ";
 			cin >> InPw;
-			if(InName == UserName && InPw == UserPw){
+			if(userCount >= 0){
+			for(int i = 0; i < userCount + 1;i++){
+			if(InName == userData[i].UserName && InPw == userData[i].UserPw){
 				cls();
 				cout << "Login Berhasil! " << endl;
 				LoggedIn = true;
-			} else{
-				cls();
-				cout << "Username/Password salah!!\nCoba lagi!!!\n";
-				AccLogin();
+			} 		
+		}
+			}else {
+			cls();
+			cout << "Username/Password salah!!\nCoba lagi!!!\n";
+			AccLogin();
 			}
-	}
+		cout << "Tidak akun yang terdaftar di database!!\n";
+		cout << "Silahkan Registrasi Akun terlebih dahulu!!!";
+		key();
+		cls();
+		LoggedIn = true;
+		AccReg();
+	}while(!LoggedIn);
+}
 	
-	void Login::AccReg(){
+	void SystemLaundry::AccReg(){
+		string inputPIN;
+		cout << "Silahkan Masukkan 5 Digit yang diberikan oleh manager : ";
+		cin >> inputPIN;	
+		if(userCount >= 0 && inputPIN == PIN) {
+		userCount++;
 		cout << "+=======================+" << endl;
 	    cout << "|     REGISTER AKUN     |" << endl;
 	    cout << "+=======================+" << endl;
 	    cout << "Masukkan Username : ";
-	    cin >> UserName;
+	    cin >> userData[userCount].UserName;
 	    cout << "Masukkan Password : ";
-	    cin >> UserPw;
+	    cin >> userData[userCount].UserPw;
+	    cout << userCount;
+
 		cls();
+	} else {
+		cout << "Error 1 : No default account detected in this app!\n";
+		LoggedIn = false;
+		key();
+		cls();
+	}
 	}
 	
 	int main(){
-		Login In;
+		SystemLaundry In;
 		cin >> In;
 		if(In.getLoggedIn()){
 			Menu M;
